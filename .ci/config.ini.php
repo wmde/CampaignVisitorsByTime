8d336534b6b4b1bcb8e8dfdcf7d9b3923ca55834
Gabriel Birke <gabriel.birke@wikimedia.de>
2025-01-22 17:51:32 +0100

    Use Woodpecker CI
    Set up DB fixtures before running tests.
    The steps for that need more than just PHP - they also need the `mysql`
    client, a running database server with permissions for the user to drop
    and create databases and a running web server.
    
    Added a configuration for Matomo to `.ci` directory

---
+++ .ci/config.ini.php
@@ -0,0 +1,22 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
+[database]
+host = "database"
+username = "root"
+password = "toor"
+dbname = "matomo"
+
+[database_tests]
+host = "database"
+username = "root"
+password = "toor"
+dbname = "matomo_t"
+
+[Development]
+enabled = 1
+disable_merged_assets = 1
+
+[tests]
+http_host = "localhost"
+port = 8080
+request_uri="/"
